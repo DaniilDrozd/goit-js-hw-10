211f5bd5a0cb2268a3bd3ff959ac5cf23081aa24
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-07-08 15:18:23 +0300

    fixed stupid mistakes,evrything seems to work

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,22 +1,37 @@
 const BASE_URL = 'https://api.thecatapi.com/v1/';
 const API_KEY =
   'live_gQ9d6291XE7YOO6cVkJSOajXrnsBkKtbEtDhuAYPvUIQwhBSvevy0efyFS2cix4J';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-export const fetchBreeds = () =>
-  fetch(`${BASE_URL}/breeds`).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
+// export const notifyFailure = errorMessage => {
+//   Notify.failure(errorMessage, {
+//     timeout: 6000,
+//     width: '550px',
+//     borderRadius: '50px',
+//     clickToClose: true,
+//     position: 'center-top',
+//     fontSize: '18px',
+//   });
+// };
+
+export const fetchBreeds = function () {
+  return fetch(`${BASE_URL}breeds?api-key=${API_KEY}`).then(response => {
+    if (!response.ok) {
+      notifyFailure(' Oops! Something went wrong! Try reloading the page!');
     }
-    return res.json();
+    return response.json();
   });
+};
 
-export const fetchCatByBreed = breedId =>
-  fetch(
-    `${BASE_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}&has_breeds=1, `
-  ).then(result => {
-    if (!result.ok) {
-      throw new Error(result.status);
-    }
-
-    return result.json();
-  });
+export const fetchCatByBreed = function (breedId) {
+  return fetch(
+    `${BASE_URL}images/search?breed_ids=${breedId}&api_key=${API_KEY}`
+  )
+    .then(response => {
+      if (!response.ok) {
+        notifyFailure('Oops! Something went wrong! Try reloading the page!');
+      }
+      return response.json();
+    })
+    .catch(err => console.log(err));
+};
--- src/js/index.js
+++ src/js/index.js
@@ -4,37 +4,29 @@ const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 const loader = document.querySelector('.loader');
 
-function fetchBreedsFromApi() {
-  fetch('https://api.example.com/breeds')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to fetch breeds');
-      }
-      return response.json();
-    })
-    .then(data => {
-      const placeholderOption = document.createElement('option');
-      placeholderOption.value = '';
-      placeholderOption.textContent = 'select a cat';
-      breedSelect.appendChild(placeholderOption);
+loader.style.display = 'none';
 
-      data.forEach(breed => {
-        const option = document.createElement('option');
-        option.textContent = breed.name;
-        option.placeholder = 'Select breed';
-        breedSelect.appendChild(option);
-      });
-    })
-    .catch(error => {
-      console.log(error);
+fetchBreeds()
+  .then(data => {
+    const placeholderOption = document.createElement('option');
+    placeholderOption.value = '';
+    placeholderOption.textContent = 'select a cat';
+    breedSelect.appendChild(placeholderOption);
+
+    data.forEach(breed => {
+      const option = document.createElement('option');
+      option.value = breed.id;
+      option.textContent = breed.name;
+      option.placeholder = 'Select breed';
+      breedSelect.appendChild(option);
     });
-}
-
-fetchBreedsFromApi();
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
 breedSelect.addEventListener('change', function () {
   loader.classList.remove('invisible');
-  clearCat();
 
   const value = breedSelect.options[breedSelect.selectedIndex].value;
   const name = breedSelect.options[breedSelect.selectedIndex].text;
@@ -55,7 +47,7 @@ breedSelect.addEventListener('change', function () {
 function createCat(cats, title) {
   const { url, breeds } = cats[0];
   const markup = breeds.reduce((markup, breed) => {
-    return `<img class="breed-image" alt="${breed.name}" src="${url}">
+    return `<img class="breed-image" alt="${breed.name}" src="${url}" width="500px">
             <div class="breed-container">
               <h2 class="breed-name">${breed.name}</h2>
               <p class="breed-temperament">${breed.temperament}</p>
