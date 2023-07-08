1d0b8f653a1b465d6cc2f47d29e46a647012c0e2
DaniilDrozd <124815236+DaniilDrozd@users.noreply.github.com>
2023-07-08 10:38:56 +0000

    Deploying to gh-pages from @ DaniilDrozd/goit-js-hw-10@fe895ec5aa268ef869e73650eaadd03085c70725 🚀

--- index.2c45c1ae.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".breed-select"),document.querySelector(".cat-info"),document.querySelector(".loader");
-//# sourceMappingURL=index.2c45c1ae.js.map
--- index.5fdafc5b.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".breed-select"),document.querySelector(".cat-info"),document.querySelector(".loader");
-//# sourceMappingURL=index.5fdafc5b.js.map
